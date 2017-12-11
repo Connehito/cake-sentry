a046619d90920f46e31bcb53a1b4165097efbc44
Hideki Kinjyo <dev@o0h.in>
2017-12-11 19:38:58 +0900

    set plugin bootstrap

---
+++ config/bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+namespace Connehito\CakeSentry;
+
+use Cake\Core\Configure;
+use Cake\Error\Middleware\ErrorHandlerMiddleware as CakeErrorHandlerMiddleware;
+use Cake\Event\EventManager;
+use Cake\Log\Log;
+use Connehito\CakeSentry\Error\ConsoleErrorHandler;
+use Connehito\CakeSentry\Error\ErrorHandler;
+use Connehito\CakeSentry\Error\Middleware\ErrorHandlerMiddleware;
+use LogicException;
+
+$isCli = PHP_SAPI === 'cli';
+if ($isCli) {
+    (new ConsoleErrorHandler(Configure::read('Error')))->register();
+} else {
+    (new ErrorHandler(Configure::read('Error')))->register();
+}
+
+$errorLogConfig = Log::getConfig('error');
+$errorLogConfig['className'] = 'Connehito\CakeSentry\Log\Engine\SentryLog';
+Log::drop('error');
+Log::setConfig('error', $errorLogConfig);
+
+$appClass = Configure::read('App.namespace') . '\Application';
+if (class_exists($appClass)) {
+    EventManager::instance()->on('Server.buildMiddleware', function ($event, $queue) {
+        /* @var \Cake\Http\MiddlewareQueue $queue */
+        $middleware = new ErrorHandlerMiddleware();
+        try {
+            $queue->insertAfter(CakeErrorHandlerMiddleware::class, $middleware);
+        } catch (LogicException $e) {
+            $queue->prepend($middleware);
+        }
+    });
+}
