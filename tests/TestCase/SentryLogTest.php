f143a86faa48ae23cb57d5ade2e73ea28176f932
Hideki Kinjyo <dev@o0h.in>
2018-02-27 22:06:11 +0900

    Tests for log engine

---
+++ tests/TestCase/SentryLogTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Connehito\CakeSentry\Test\TestCase;
+
+use Cake\Core\Configure;
+use Connehito\CakeSentry\Http\Client;
+use Connehito\CakeSentry\Log\Engine\SentryLog;
+use PHPUnit\Framework\TestCase;
+use ReflectionProperty;
+
+class SentryLogTest extends TestCase
+{
+    /** @var SentryLog */
+    private $subject;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        Configure::write('Sentry.dsn', 'https://user:pass@example.com/yourproject');
+        $subject = new SentryLog([]);
+
+        $clientMock = $this->getMockBuilder(Client::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->subject = $subject;
+
+        $clientProp = $this->getClientProp();
+        $clientProp->setValue($this->subject, $clientMock);
+    }
+
+    /**
+     * Test for log()
+     */
+    public function testLog()
+    {
+        $level = E_USER_ERROR;
+        $message = 'something wrong';
+        $context = [];
+
+        $client = $this->getClientProp()->getValue($this->subject);
+        $client->expects($this->once())
+            ->method('capture')
+            ->with($level, $message, $context);
+
+        $this->subject->log($level, $message, $context);
+    }
+
+    /**
+     * Helper access subject::$client(reflection)
+     *
+     * @return ReflectionProperty Client reflection
+     */
+    private function getClientProp()
+    {
+        $clientProp = new ReflectionProperty($this->subject, 'client');
+        $clientProp->setAccessible(true);
+
+        return $clientProp;
+    }
+}
