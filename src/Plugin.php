5f7cd65be7bc6ab9eee715b6ae9d0f856b5d9904
Hideki Kinjyo <dev@o0h.in>
2019-07-22 21:45:37 +0900

    Load middleware in Application::pluginMiddleware()

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,7 +3,35 @@
 namespace Connehito\CakeSentry;
 
 use Cake\Core\BasePlugin;
+use Cake\Core\Configure;
+use Cake\Error\Middleware\ErrorHandlerMiddleware as CakeErrorHandlerMiddleware;
+use Cake\Event\EventManager;
+use Cake\Http\MiddlewareQueue;
+use Connehito\CakeSentry\Error\Middleware\ErrorHandlerMiddleware;
+use LogicException;
 
 class Plugin extends BasePlugin
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function middleware($middleware)
+    {
+        $middleware = parent::middleware($middleware);
+
+        $appClass = Configure::read('App.namespace') . '\Application';
+        if (class_exists($appClass)) {
+            EventManager::instance()->on('Server.buildMiddleware', function ($event, $queue) {
+                /* @var MiddlewareQueue $queue */
+                $middleware = new ErrorHandlerMiddleware();
+                try {
+                    $queue->insertAfter(CakeErrorHandlerMiddleware::class, $middleware);
+                } catch (LogicException $e) {
+                    $queue->prepend($middleware);
+                }
+            });
+        }
+
+        return $middleware;
+    }
 }
