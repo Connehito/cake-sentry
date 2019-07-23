20fc9e89f32e4399ef678e7c47025ad61beff040
Hideki Kinjyo <dev@o0h.in>
2019-07-23 23:40:35 +0900

    Cleanup ErrorHandlerMiddleware setup process.
    Remove two.
    1. To check if Application class exists
    2. Unnecessary trap for throwing of MiddlewareQueue::insertAfter's LogicException

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,12 +3,10 @@
 namespace Connehito\CakeSentry;
 
 use Cake\Core\BasePlugin;
-use Cake\Core\Configure;
 use Cake\Error\Middleware\ErrorHandlerMiddleware as CakeErrorHandlerMiddleware;
 use Cake\Event\EventManager;
 use Cake\Http\MiddlewareQueue;
 use Connehito\CakeSentry\Error\Middleware\ErrorHandlerMiddleware;
-use LogicException;
 
 class Plugin extends BasePlugin
 {
@@ -19,18 +17,11 @@ class Plugin extends BasePlugin
     {
         $middleware = parent::middleware($middleware);
 
-        $appClass = Configure::read('App.namespace') . '\Application';
-        if (class_exists($appClass)) {
-            EventManager::instance()->on('Server.buildMiddleware', function ($event, $queue) {
-                /* @var MiddlewareQueue $queue */
-                $middleware = new ErrorHandlerMiddleware();
-                try {
-                    $queue->insertAfter(CakeErrorHandlerMiddleware::class, $middleware);
-                } catch (LogicException $e) {
-                    $queue->prepend($middleware);
-                }
-            });
-        }
+        EventManager::instance()->on('Server.buildMiddleware', function ($event, $queue) {
+            /* @var MiddlewareQueue $queue */
+            $middleware = new ErrorHandlerMiddleware();
+            $queue->insertAfter(CakeErrorHandlerMiddleware::class, $middleware);
+        });
 
         return $middleware;
     }
