3041a66fa4660cbed0ba7dcf16505c6f95064c68
Hideki Kinjyo <mail@o0h.in>
2019-11-03 19:28:32 +0900

    Add web-error/exception action

--- tests/test_app/app/src/Controller/PagesController.php
+++ tests/test_app/app/src/Controller/PagesController.php
@@ -14,10 +14,8 @@
  */
 namespace App\Controller;
 
-use Cake\Core\Configure;
-use Cake\Http\Exception\ForbiddenException;
-use Cake\Http\Exception\NotFoundException;
-use Cake\View\Exception\MissingTemplateException;
+use Cake\Http\Exception\BadRequestException;
+use Cake\Utility\Inflector;
 
 /**
  * Static content controller
@@ -30,40 +28,51 @@ class PagesController extends AppController
 {
 
     /**
-     * Displays a view
+     * Render specify error
      *
-     * @param array ...$path Path segments.
-     * @return \Cake\Http\Response|null
-     * @throws \Cake\Http\Exception\ForbiddenException When a directory traversal attempt.
-     * @throws \Cake\Http\Exception\NotFoundException When the view file could not
-     *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
+     * like: http://127.0.0.1:8080/error/warning/411?message=something-wrong
+     *
+     * @param string $errorName
+     * @param int $errorCode
      */
-    public function display(...$path)
+    public function error(string $errorName, int $errorCode=400)
     {
-        $count = count($path);
-        if (!$count) {
-            return $this->redirect('/');
-        }
-        if (in_array('..', $path, true) || in_array('.', $path, true)) {
-            throw new ForbiddenException();
-        }
-        $page = $subpage = null;
+        $gottenError = (function (string $errorName) : ?int {
+            $key = strtoupper($errorName);
+            $constName = "E_USER_{$key}";
 
-        if (!empty($path[0])) {
-            $page = $path[0];
-        }
-        if (!empty($path[1])) {
-            $subpage = $path[1];
-        }
-        $this->set(compact('page', 'subpage'));
+            return constant($constName) ?: E_USER_NOTICE;
+        })($errorName);
+        $message = $this->getRequest()->getQuery('message', 'error!');
+        trigger_error($message, $gottenError);
 
-        try {
-            $this->render(implode('/', $path));
-        } catch (MissingTemplateException $exception) {
-            if (Configure::read('debug')) {
-                throw $exception;
-            }
-            throw new NotFoundException();
-        }
+        $this->set(compact('gottenError', 'errorName', 'errorCode', 'message', ));
+
+        $this->setResponse(
+            $this->getResponse()->withStatus($errorCode)
+        );
+        $this->render('dump_vars');
+    }
+
+    /**
+     * Throw specify exception
+     *
+     * like: http://127.0.0.1:8080/exception/not-found/404?message=page-not-found
+     *
+     * @param string $exceptionName
+     * @param int $errorCode
+     */
+    public function exception(string $exceptionName, int $errorCode=400)
+    {
+        $gottenException = (function (string $exceptionName) : ?string {
+            $baseName = Inflector::classify($exceptionName);
+            $fqcn = "\\Cake\\Http\\Exception\\{$baseName}Exception";
+
+            return class_exists($fqcn) ? $fqcn : BadRequestException::class;
+        })($exceptionName);
+
+        $message = $this->getRequest()->getQuery('message', 'exception!');
+
+        throw new $gottenException($message, $errorCode);
     }
 }
