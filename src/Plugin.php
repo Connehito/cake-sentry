afb35b485f4f5070bd5924413042c53a23e415a4
Benjamin Gehrels <github@gehrels.info>
2020-01-07 18:12:52 +0100

    Update Middleware signature to match parent class

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,6 +3,7 @@
 namespace Connehito\CakeSentry;
 
 use Cake\Core\BasePlugin;
+use Cake\Http\MiddlewareQueue;
 use Cake\Error\Middleware\ErrorHandlerMiddleware as CakeErrorHandlerMiddleware;
 use Connehito\CakeSentry\Error\Middleware\ErrorHandlerMiddleware;
 
@@ -11,7 +12,7 @@ class Plugin extends BasePlugin
     /**
      * {@inheritDoc}
      */
-    public function middleware($middleware)
+    public function middleware(MiddlewareQueue $middleware)
     {
         $middleware = parent::middleware($middleware);
         $middleware->insertAfter(
