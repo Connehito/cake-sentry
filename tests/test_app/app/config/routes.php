97ad8850fdf79da01471e548b78772610e40495e
o0h <dev@o0h.in>
2020-05-09 12:32:26 +0900

    Implements error/exception sandbox action
    see: https://github.com/Connehito/cake-sentry/pull/16

--- tests/test_app/app/config/routes.php
+++ tests/test_app/app/config/routes.php
@@ -57,32 +57,10 @@ $routes->scope('/', function (RouteBuilder $builder) {
      */
     $builder->applyMiddleware('csrf');
 
-    /*
-     * Here, we are connecting '/' (base path) to a controller called 'Pages',
-     * its action called 'display', and we pass a param to select the view file
-     * to use (in this case, templates/Pages/home.php)...
-     */
     $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
-
-    /*
-     * ...and connect the rest of 'Pages' controller's URLs.
-     */
     $builder->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
-
-    /*
-     * Connect catchall routes for all controllers.
-     *
-     * The `fallbacks` method is a shortcut for
-     *
-     * ```
-     * $builder->connect('/:controller', ['action' => 'index']);
-     * $builder->connect('/:controller/:action/*', []);
-     * ```
-     *
-     * You can remove these routes once you've connected the
-     * routes you want in your application.
-     */
-    $builder->fallbacks();
+    $builder->connect('/error/*', ['controller' => 'Pages', 'action' => 'error']);
+    $builder->connect('/exception/*', ['controller' => 'Pages', 'action' => 'exception']);
 });
 
 /*
--- tests/test_app/app/src/Controller/PagesController.php
+++ tests/test_app/app/src/Controller/PagesController.php
@@ -17,9 +17,11 @@ declare(strict_types=1);
 namespace TestApp\Controller;
 
 use Cake\Core\Configure;
+use Cake\Http\Exception\BadRequestException;
 use Cake\Http\Exception\ForbiddenException;
 use Cake\Http\Exception\NotFoundException;
 use Cake\Http\Response;
+use Cake\Utility\Inflector;
 use Cake\View\Exception\MissingTemplateException;
 
 /**
@@ -32,16 +34,71 @@ use Cake\View\Exception\MissingTemplateException;
 class PagesController extends AppController
 {
     /**
+     * Render specify error
+     *
+     * like: http://127.0.0.1:8080/error/warning/411?message=something-wrong
+     *
+     * @param string $errorName
+     * @param string $errorCode
+     */
+    public function error(string $errorName, $errorCode = '400')
+    {
+        if (is_numeric($errorCode)) {
+            $errorCode = (int)$errorCode;
+        }
+        $gottenError = (function (string $errorName): ?int {
+            $key = strtoupper($errorName);
+            $constName = "E_USER_{$key}";
+
+            return constant($constName) ?: E_USER_NOTICE;
+        })($errorName);
+        $message = $this->getRequest()->getQuery('message', 'error!');
+        trigger_error($message, $gottenError);
+
+        $this->set(compact('gottenError', 'errorName', 'errorCode', 'message'));
+
+        $this->setResponse(
+            $this->getResponse()->withStatus($errorCode)
+        );
+        $this->render('dump_vars');
+    }
+
+    /**
+     * Throw specify exception
+     *
+     * like: http://127.0.0.1:8080/exception/not-found/404?message=page-not-found
+     *
+     * @param string $exceptionName
+     * @param string $errorCode
+     */
+    public function exception(string $exceptionName, $errorCode = '400')
+    {
+        if (is_numeric($errorCode)) {
+            $errorCode = (int)$errorCode;
+        }
+        $gottenException = (function (string $exceptionName): ?string {
+            $baseName = Inflector::classify($exceptionName);
+            $fqcn = "\\Cake\\Http\\Exception\\{$baseName}Exception";
+
+            return class_exists($fqcn) ? $fqcn : BadRequestException::class;
+        })($exceptionName);
+
+        $message = $this->getRequest()->getQuery('message', 'exception!');
+
+        throw new $gottenException($message, $errorCode);
+    }
+
+    /**
      * Displays a view
      *
      * @param array ...$path Path segments.
-     * @return \Cake\Http\Response|null
-     * @throws \Cake\Http\Exception\ForbiddenException When a directory traversal attempt.
-     * @throws \Cake\View\Exception\MissingTemplateException When the view file could not
+     * @return Response|null
+     * @throws ForbiddenException When a directory traversal attempt.
+     * @throws MissingTemplateException When the view file could not
      *   be found and in debug mode.
-     * @throws \Cake\Http\Exception\NotFoundException When the view file could not
+     * @throws NotFoundException When the view file could not
      *   be found and not in debug mode.
-     * @throws \Cake\View\Exception\MissingTemplateException In debug mode.
+     * @throws MissingTemplateException In debug mode.
      */
     public function display(...$path): ?Response
     {
