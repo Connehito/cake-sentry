1f6d7ad73b778ab331b2c61d30b5ec51a54f7b76
o0h <dev@o0h.in>
2021-04-10 21:16:10 +0900

    Add Sentry context cakephp-ver.

--- tests/test_app/app/src/Error/Event/SentryErrorContext.php
+++ tests/test_app/app/src/Error/Event/SentryErrorContext.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace TestApp\Error\Event;
 
+use Cake\Core\Configure;
 use Cake\Event\Event;
 use Cake\Event\EventListenerInterface;
 use Cake\Http\ServerRequest;
@@ -41,6 +42,7 @@ class SentryErrorContext implements EventListenerInterface
 
             sentryConfigureScope(function (Scope $scope) use ($request, $event) {
                 $scope->setTag('app_version', $request->getHeaderLine('App-Version') ?: '1.0');
+                $scope->setTag('cakephp_version', Configure::version());
                 $exception = $event->getData('exception');
                 if ($exception) {
                     assert($exception instanceof \Throwable);
